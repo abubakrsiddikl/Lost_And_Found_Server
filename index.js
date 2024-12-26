f02a7064abd6587eef5e6358784e0b248b5595c1
limon <limonsk026@gmail.com>
2024-12-26 12:52:03 +0600

    jwt token verify api successfull

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+// veryfiToken
+const veryfiToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "UnAuthorized Access" });
+    }
+    req.user = decoded;
+    // console.log(req.user)
+    next();
+  });
+
+};
 
 // root server
 app.get("/", (req, res) => {
@@ -47,6 +72,31 @@ async function run() {
     const recoveredItemsCollection = client
       .db("lostAndFoundItemsCollection")
       .collection("recoveredItems");
+
+    // jwt authentication related apis
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // after logout to delet token to cookie
+    app.post("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     // items related apis
     // get all item to database
     app.get("/allItems", async (req, res) => {
@@ -89,8 +139,12 @@ async function run() {
     });
 
     // get specified user added item
-    app.get("/myItems/:email", async (req, res) => {
+    app.get("/myItems/:email", veryfiToken, async (req, res) => {
       const userEmail = req.params.email;
+      // console.log(req.cookies?.token)
+      if (req.user.email !== userEmail) {
+        return res.status(403).send({message: "Forbidden"})
+      }
       const query = { email: userEmail };
       const result = await lostAndFoundItemsCollection.find(query).toArray();
       res.send(result);
